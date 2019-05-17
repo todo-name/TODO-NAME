28064cde55b46d068377a9f653393377ece32c02
my828 <minyang828@gmail.com>
2019-05-16 17:10:31 -0700

    starter

---
+++ todo/src/Landing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+
+export default class Landing extends Component {
+    render() {
+        return(
+            <div>
+                <NavBar />
+            </div>
+        )
+    }
+}
---
+++ todo/src/NavBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class NavBar extends Component {
+    render() {
+        return(
+            <nav>
+                <button>
+                    Log In
+                </button>
+                <button>
+                    Sign Up
+                </button>
+            </nav>
+        )
+    }
+}
